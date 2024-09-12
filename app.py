22c73c4f2a602b79f6cab57f3c2ca20e91050a87
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2024-09-12 01:57:58 -0400

    Messaging Optimization

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_socketio import SocketIO, emit
+from flask_socketio import emit
 from sqlalchemy.exc import SQLAlchemyError
-from blackjack import BlackJackGameManager
-from models import db, BlackjackPlayer, initialize_scoped_session, get_scoped_session
+from blackjack.game_manager import BlackJackGameManager
+from models import db, initialize_scoped_session, get_scoped_session, User
+import blackjack.utility as u
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -14,21 +15,12 @@ db.init_app(app)
 with app.app_context():
     initialize_scoped_session()
 
-from blackjack import BlackJackGameManager
 from flask_socketio import SocketIO
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 game_manager = BlackJackGameManager(app, socketio)
 
 
-def get_leaderboard():
-    leaderboard = []
-    try:
-        leaderboard = BlackjackPlayer.query.order_by(BlackjackPlayer.balance.desc()).all()
-    except:
-        pass
-    finally:
-        return leaderboard
 
 
 ##-----------------------------------------------------
@@ -36,26 +28,36 @@ def get_leaderboard():
 ##-----------------------------------------------------
 @app.route('/')
 def index():
-    player_id = session.get('player_id')
-    player = BlackjackPlayer.query.get(player_id)
+    scoped_session = get_scoped_session()
+    user_id = session.get('user_id')
+    user = scoped_session.get(User, user_id)
+    leaderboard = scoped_session.query(User).order_by(User.balance).all()
     return render_template('index.html',
         games=list(game_manager.games.items()),
-        player=player,
-        leaderboard=get_leaderboard()
+        player=user,
+        leaderboard=leaderboard
     )
 @app.route('/game/<game_id>')
 def game(game_id):
-    game = game_manager.get_or_create_game(game_id)
-    player_id = session.get('player_id')
-    player = BlackjackPlayer.query.get(player_id)
-    if game:
-        session['game_id'] = game.id
+    if not u.valid_game_id(game_id):
+        return redirect(url_for('index'))
+
+    game = game_manager.get_game(game_id)
+    if not game:
+        return redirect(url_for('index'))
+
+    scoped_session = get_scoped_session()
+    user_id = session.get('user_id')
+    user = scoped_session.get(User, user_id)
+    session['game_id'] = game.id
+    leaderboard = scoped_session.query(User).order_by(User.balance).all()
 
     return render_template('game.html',
-                           game=game,
-                           player_required=True,
-                           player=player,
-        leaderboard=get_leaderboard())
+       game=game,
+       player_required=True,
+       player=user,
+       leaderboard=leaderboard
+    )
 @app.route('/privacy')
 def privacy():
     return render_template('privacy.html')
@@ -81,10 +83,10 @@ def login():
             response['errors'].append('Username and Password must be provided')
             return jsonify(response), 400
 
-        player = BlackjackPlayer.query.filter_by(username=username).first()
+        player = User.query.filter_by(username=username).first()
         if player:
             if check_password_hash(player.password, password):
-                session['player_id'] = player.id
+                session['user_id'] = player.id
                 response['success'] = True
                 return jsonify(response), 200
             else:
@@ -113,16 +115,16 @@ def signup():
             response['errors'].append('Name, Username, and Password must be provided')
             return jsonify(response), 400
 
-        if BlackjackPlayer.query.filter_by(username=username).first():
+        if User.query.filter_by(username=username).first():
             response['errors'].append('User already exists')
             return jsonify(response), 400
 
         hashed_password = generate_password_hash(password)
-        new_player = BlackjackPlayer(username=username, name=name, password=hashed_password, balance=5000)
+        new_player = User(username=username, name=name, password=hashed_password, balance=5000)
         db.session.add(new_player)
         db.session.commit()
 
-        session['player_id'] = new_player.id
+        session['user_id'] = new_player.id
         response['success'] = True
         return jsonify(response), 201
 
@@ -143,101 +145,51 @@ def logout():
 ##-----------------------------------------------------
 @socketio.on('connect')
 def handle_connect():
-    player_id = session.get('player_id')
     game_id = session.get('game_id')
-    if player_id:
-        player = BlackjackPlayer.query.get(player_id)
-        if player:
-            player.sid = request.sid
-            if game_id:
-                player.game_id = game_id
-            db.session.commit()
+    user_id = session.get('user_id')
+    game_manager.connect_game(game_id, user_id, request.sid)
 @socketio.on('disconnect')
 def handle_disconnect():
-    player = None
-    game = None
-
-    player_id = session.get('player_id')
     game_id = session.get('game_id')
-
-    if player_id:
-        player = BlackjackPlayer.query.get(player_id)
-
-    if game_id:
-        game = game_manager.get_game(game_id)
-
-    if player:
-        player.sid = None
-        player.game_id = None
-        db.session.commit()
-        if game:
-            game_manager.leave_game(game, player)
+    user_id = session.get('user_id')
+    game_manager.leave_game(game_id, user_id)
 @socketio.on('join_game')
-def handle_join_game(data):
-
-    # Get the Game
+def handle_join_game(role):
     game_id = session.get('game_id')
-    game = game_manager.get_or_create_game(game_id)
-    if not game:
-        emit('error', {'message': 'Unable to create game'})
-        return
+    user_id = session.get('user_id')
+    seat_id, error = game_manager.join_game(game_id, user_id)
 
-    # Get the Player
-    player_id = session.get('player_id')
-    player = BlackjackPlayer.query.get(player_id)
-    if not player:
-        emit('error', {'message': 'Player not found'})
-        return
-
-    # Join the Game
-    game_manager.join_game(game, player)
+    emit('join_game', {
+        'success': not not seat_id,
+        'seat_id': seat_id,
+        'error': error,
+        'role': role
+    })
 @socketio.on('player_action')
 def handle_player_action(action):
-
-    # Get the Action
-    if not action:
-        emit('error', {'message': 'Invalid action'})
-        return
-
-    # Get the Game
     game_id = session.get('game_id')
-    game = game_manager.get_game(game_id)
-    if not game:
-        emit('error', {'message': 'Game not found'})
-        return
+    user_id = session.get('user_id')
+    success, error = game_manager.player_action(game_id, user_id, action)
 
-    # Get the Player
-    player_id = session.get('player_id')
-    player = BlackjackPlayer.query.get(player_id)
-    if not player:
-        emit('error', {'message': 'Player not found'})
-        return
-
-    game_manager.player_action(game, player, action)
+    emit('player_action', {
+        'success': success,
+        'error': error,
+        'action': action
+    })
 @socketio.on('player_bet')
-def handle_player_bet(data):
-    # Get the Game
+def handle_player_bet(amount):
     game_id = session.get('game_id')
-    game = game_manager.get_game(game_id)
-    if not game:
-        emit('error', {'message': 'Game not found'})
-        return
+    user_id = session.get('user_id')
+    success, error = game_manager.player_bet(game_id, user_id, amount)
 
-    # Get the Player
-    player_id = session.get('player_id')
-    db_session = get_scoped_session()
-    player = db_session.query(BlackjackPlayer).get(player_id)
-    if not player:
-        emit('error', {'message': 'Player not found'})
-        return
+    emit('player_bet', {
+        'success': success,
+        'error': error,
+        'amount': amount
+    })
 
-    # Get the Amount
-    amount = data.get('amount')
-    if not amount:
-        emit('error', {'message': 'Invalid amount'})
-        return
 
-    game_manager.player_bet(game, player, amount)
+
 
 
 if __name__ == '__main__':
